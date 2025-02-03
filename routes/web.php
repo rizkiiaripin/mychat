b8a15fed6a766a60246c0fc20e315a75a418f849
rizkiiaripin <rizkixirpl@gmail.com>
2025-02-03 09:57:54 +0700

    delete event and configurate in env

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Events\messageCreated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    messageCreated::dispatch('haidsid');
     return view('welcome');
 });
